8d6465a1339cb27652e756721710c6223ba794cc
Stenar <stenar.laane@voco.ee>
2025-03-20 09:10:14 +0200

    Algne versioon mängust

--- pyGame1.py
+++ pyGame1.py
@@ -1,1 +1,20 @@
 import pygame
+
+background_color = (100, 7, 163)
+screen_width = 800
+screen_height = 600
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Bubble Blaster!")
+
+screen.fill(background_color)
+
+pygame.display.flip()
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.quit:
+            running = False
+pygame.quit()
